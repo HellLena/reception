fa004d087b3da7e570e2b87e2a7759ef9a1e2804
HellLena <longlivehard.n.heavy@gmail.com>
2015-09-04 12:53:16 +0300

    style the login form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,4 +21,19 @@
 
 $(document).on('ready page:change', function() {
     $(".bootstrap_flash").delay(5000).fadeOut(200);
-})
+});
+
+$(document).ready(function() {
+    $('*[required="required"]').on('change invalid', function() {
+        var textfield = $(this).get(0);
+
+        // 'setCustomValidity not only sets the message, but also marks
+        // the field as invalid. In order to see whether the field really is
+        // invalid, we have to remove the message first
+        textfield.setCustomValidity('');
+
+        if (!textfield.validity.valid) {
+            textfield.setCustomValidity('Это поле должно быть заполнено');
+        }
+    });
+});
--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
-    $('.required').on('change invalid', function() {
-        var textfield = $(this).get(0);
-
-        // 'setCustomValidity not only sets the message, but also marks
-        // the field as invalid. In order to see whether the field really is
-        // invalid, we have to remove the message first
-        textfield.setCustomValidity('');
-
-        if (!textfield.validity.valid) {
-            textfield.setCustomValidity('Это поле должно быть заполнено');
-        }
-    });
 
     $("button[data-target='#appeal_type']").click(function(){
         $('#appeal_type').modal('toggle');
